cde10d59c511005c3819249ed350df1ea1290be9
rafli <49984708+icecreameu@users.noreply.github.com>
2020-05-07 16:05:03 +0800

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,5 @@
 export default {
-  nicknames: ["Ava", "Babe", "Sayang", "Avakuu", "Cintuu"],
+  nicknames: ["Nuna", "Ryujin", "Yuyu", "Nunakuuu", "Yuyukuu"],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
