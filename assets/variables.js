70236f9caab8c5fc634f9e6ff132ee93bff66b94
rafli <49984708+icecreameu@users.noreply.github.com>
2020-05-07 16:06:25 +0800

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,10 +1,10 @@
 export default {
   nicknames: ["Nuna", "Ryujin", "Yuyu", "Nunakuuu", "Yuyukuu"],
   greetings: {
-    evening: "Good Evening",
-    afternoon: "Good Afternoon",
-    day: "Good Day",
-    morning: "Good Morning",
-    night: "Good Night"
+    evening: "Selamat Malam",
+    afternoon: "Selamat Sore",
+    day: "Selamat Siang",
+    morning: "Selamat Pagi",
+    night: "Selamat Tidur,Semoga mimpi indah"
   }
 };
